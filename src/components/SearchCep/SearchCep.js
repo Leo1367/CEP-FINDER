6b4b990cd965ca74dc08fd418c45f28ced7296b3
Leo1367 <leonardoherculano136@gmail.com>
2023-10-23 00:20:40 -0300

    att

--- src/components/SearchCep/SearchCep.js
+++ src/components/SearchCep/SearchCep.js
@@ -22,7 +22,6 @@ export default function SearchCep() {
           const responseData = response.data;
 
           navigate('/result', { state: { props: responseData } });
-          console.log(responseData)
         } catch (error) {
           console.error('Erro ao encontrar o CEP: ', error);
           
@@ -55,3 +54,59 @@ export default function SearchCep() {
         </main>
     )
 }
+
+/* 
+import { FaSearch } from "react-icons/fa";
+import styles from '../../styles/App.module.css';
+import style from './SearchCep.module.css';
+import React, { useState } from 'react';
+import axios from "axios";
+import Informations from "../Informations/Informations.js";
+
+export default function SearchCep(props) {
+    const [cep, setCep] = useState('');
+
+    const handleCepChange = (e) => {
+        let inputValue = e.target.value;
+        inputValue = inputValue.replace(/[^\d]/g, '');
+        inputValue.length <= 5 ? inputValue = inputValue.replace(/^(\d{5})/, '$1') : inputValue = inputValue.replace(/^(\d{5})(\d{0,3})/, '$1-$2');
+        setCep(inputValue);
+    };
+
+    const fetchCepInfo = async () => {
+        try {
+            const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+            const responseData = response.data;
+            props.onCepData(responseData);
+        } catch (error) {
+            console.error('Erro ao encontrar o CEP: ', error);
+        }
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        fetchCepInfo();
+    };
+
+    return (
+        <main className={styles.container}>
+            <div>
+                <form className={style.searchBox} onSubmit={handleSubmit}>
+                    <FaSearch color="#989898" className={style.searchIcon} />
+                    <input
+                        type="text"
+                        placeholder="Informe o CEP aqui"
+                        className={style.searchTerm}
+                        value={cep}
+                        onChange={handleCepChange}
+                        maxLength="9"
+                    />
+                    <button type="submit" className={style.searchButton}>
+                        Buscar
+                    </button>
+                </form>
+            </div>
+        </main>
+    )
+}
+ */
